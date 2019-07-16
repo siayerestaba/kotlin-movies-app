1f00594d3709749f09212b58d9cba67148f01514
Ilia <ilia.berlana@gmail.com>
2019-07-17 00:49:01 +0200

    Refactor use coroutines

--- app/src/main/java/com/iliaberlana/movies/ui/presenters/MainPresenter.kt
+++ app/src/main/java/com/iliaberlana/movies/ui/presenters/MainPresenter.kt
@@ -7,16 +7,13 @@ import com.iliaberlana.movies.ui.commons.logDebug
 import com.iliaberlana.movies.ui.model.toUIMovie
 import com.iliaberlana.movies.ui.presenters.views.ViewMain
 import com.iliaberlana.movies.usecases.ListMovies
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class MainPresenter(
     private val listMovies: ListMovies
-) {
+): CoroutineScope by MainScope() {
     var view: ViewMain? = null
-    private var page: Int = 1
+    var page: Int = 1
 
     fun create() {
         renderMovies()
@@ -28,31 +25,31 @@ class MainPresenter(
         renderMovies()
     }
 
-    private fun renderMovies() {
+    private fun renderMovies() = launch {
         view?.showLoading()
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val resultMovies = withContext(Dispatchers.IO) { listMovies(page) }
-            view?.hideLoading()
+        val resultMovies = withContext(Dispatchers.IO) { listMovies(page) }
+        view?.hideLoading()
 
-            when (resultMovies) {
-                is Either.Right -> {
-                    view?.hideErrorCase()
-                    view?.listMovies(resultMovies.b.map {it.toUIMovie()})
-                }
-                is Either.Left -> {
-                    when (resultMovies.a) {
-                        is DomainError.NoInternetConnectionException -> showErrorMessage(R.string.noInternetConectionError)
-                        is DomainError.NoMoreMoviesException -> showErrorMessage(R.string.noMoreMovieError)
-                        is DomainError.UnknownException -> showErrorMessage(R.string.emptyList)
-                    }
+        when (resultMovies) {
+            is Either.Right -> {
+                view?.hideErrorCase()
+                view?.listMovies(resultMovies.b.map { it.toUIMovie() })
+            }
+            is Either.Left -> {
+                when (resultMovies.a) {
+                    is DomainError.NoInternetConnectionException -> showErrorMessage(R.string.noInternetConectionError)
+                    is DomainError.NoMoreMoviesException -> showErrorMessage(R.string.noMoreMovieError)
+                    is DomainError.UnknownException -> showErrorMessage(R.string.emptyList)
                 }
             }
         }
+
+        Unit
     }
 
     private fun showErrorMessage(stringIdError: Int) {
-        if(page == 1) {
+        if (page == 1) {
             view?.showErrorCase(stringIdError)
         } else {
             view?.showToastMessage(stringIdError)
